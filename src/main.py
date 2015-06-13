34c9eb957553442b3b030e5b89302b2d88de4ee0
Marius <mgerdes@uni-potsdam.de>
2015-06-13 16:46:27 +0200

    Added python main source file

---
+++ src/main.py
@@ -0,0 +1,13 @@
+# main.py
+
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty,\
+    ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+
+import 
+
+if(__name__ == "__main__"):

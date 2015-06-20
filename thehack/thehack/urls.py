9937c196b7859826ad67b2fd2b5562b920333900
Marius <mgerdes@uni-potsdam.de>
2015-06-20 22:19:02 +0200

    Added working /mtest/<string>/

--- thehack/thehack/urls.py
+++ thehack/thehack/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'thehack.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^mtest/', include('mtest.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
